959158639f5f7be7fcbacfa81f2ea36cd7ba616a
Leo Furze-Waddock <leo@furze-waddock.com>
2026-03-31 14:49:26 +0100

    test: workspace project exchange jest config

---
+++ apps/exchange/jest.config.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  displayName: 'exchange',
+  testEnvironment: 'jest-environment-node',
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx,ts,tsx}', '!<rootDir>/src/**/*.d.ts'],
+};
