2551311e6added464ce90ef3f76a6772864f121f
Leo Furze-Waddock <leo@furze-waddock.com>
2026-03-31 14:48:37 +0100

    test: workspace project gekko jest config

--- apps/gekko/jest.config.cjs
+++ apps/gekko/jest.config.cjs
@@ -1,8 +1,13 @@
-const path = require('path')
-
 module.exports = {
+  displayName: 'gekko',
   testEnvironment: 'jest-environment-node',
   testMatch: ['**/test/**/*.jest.js'],
-  collectCoverage: true,
-  coverageReporters: ["json", "html"],
-}
+  collectCoverageFrom: [
+    '<rootDir>/core/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/strategies/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/plugins/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/web/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/exchanges/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/**/*.d.ts',
+  ],
+};
